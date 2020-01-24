37db0e9eec1508a76dc09c1978325ebdc9b9dd51
Rached <bch.rached@gmail.com>
2020-01-24 10:18:38 +0100

    Project basic settings

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Index extends React.Component<any> {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
